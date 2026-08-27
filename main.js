1b9cd8cd054d7f8752f54590e8bf8734363da476
kelvinyhf <yhf719@gmail.com>
2026-08-27 11:00:59 +0800

    add: starr and obstacles

--- main.js
+++ main.js
@@ -1,33 +1,50 @@
-// main.js
 import kaplay from "./assets/libraries/kaplay.mjs";
-kaplay({
-  background: "#4d9564",
+const k = kaplay({
   canvas: document.getElementById("canvas"),
+  background: "#333333",
+  fullscreen: true,
 });
 
-loadBean();
-scene("bean", () => {
+// Load sprites
+k.loadSprite("starr", "./assets/images/star-o.png");
+k.loadSprite("cursor", "./assets/images/cursor.png");
+k.loadSprite("steel", "./assets/images/steel.png");
 
-  const player = add([
-    sprite("bean"),
-    pos(center())
+// Cursor
+k.setCursor("url('./assets/images/cursor.png'), auto");
+
+// Starr
+const starr = k.add([
+  k.sprite("starr"),
+  k.pos(width() * 0.5, height() * 0.8),
+  k.anchor("center"),
+  k.area(),
+]);
+
+starr.onUpdate(() => {
+  const mousePos = k.toWorld(k.mousePos());
+  starr.pos.x = k.lerp(starr.pos.x, mousePos.x, 0.05);
+});
+
+// Obstacles
+k.loop(1.5, () => {
+
+  const steel = k.add([
+    k.sprite("steel"),
+    k.pos(k.rand(0, k.width()), -50),
+    k.anchor("center"),
+    k.area(),
+    "obstacle"
   ]);
 
-  onKeyDown("up", () => {
-    player.move(0, -200);
+  steel.onUpdate(() => {
+    steel.pos.y += 200 * k.dt();
+    if (steel.pos.y > k.height() + 50) steel.destroy();
   });
 
-  onKeyDown("right", () => {
-    player.move(200, 0);
-  });
-
-  onKeyDown("down", () => {
-    player.move(0, 200);
-  });
-
-  onKeyDown("left", () => {
-    player.move(-200, 0);
-  });
-  
 });
-go("bean");
+
+starr.onCollide("obstacle", (obstacle) => {
+  obstacle.destroy();
+  addKaboom(toWorld(mousePos()));
+});
