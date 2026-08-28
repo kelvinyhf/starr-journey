bb497e6f1ddfcbfa171cb078105b71827c3bab7e
kelvinyhf <yhf719@hotmail.com>
2026-08-28 20:06:02 +0800

    style: new coin, rocks, font, and stars

--- main.js
+++ main.js
@@ -16,8 +16,32 @@ k.setCursor("url('./assets/sprites/cursor.png'), auto");
 // Load sprites
 // ------------------------------
 k.loadSprite("starr", "./assets/sprites/star-o.png");
-k.loadSprite("steel", "./assets/sprites/steel.png");
-k.loadSprite("coin", "./assets/sprites/coin-o.png");
+k.loadSprite("rock-sm", "./assets/sprites/rock-sm.png");
+k.loadSprite("rock-md", "./assets/sprites/rock-md.png");
+k.loadSprite("rock-lg", "./assets/sprites/rock-lg.png");
+k.loadSprite(
+  "coin",
+  [
+    "./assets/sprites/coin/frame1.png",
+    "./assets/sprites/coin/frame2.png",
+    "./assets/sprites/coin/frame3.png",
+    "./assets/sprites/coin/frame4.png",
+    "./assets/sprites/coin/frame5.png",
+    "./assets/sprites/coin/frame6.png",
+    "./assets/sprites/coin/frame7.png",
+    "./assets/sprites/coin/frame8.png"
+  ],
+  {
+    anims: {
+      idle: {
+        from: 0,
+        to: 7,
+        speed: 8,
+        loop: true
+      }
+    }
+  }
+);
 
 // ------------------------------
 // Starr
@@ -45,31 +69,101 @@ starr.onUpdate(() => {
 });
 
 // ------------------------------
-// Obstacles
+// Stars Background
+// ------------------------------
+k.loop(0.1, () => {
+  const randomSpeed = k.rand(500, 650);
+  const randomRadius = k.rand(1.5, 3);
+  const randomOpacity = k.rand(0, 0.75);
+  const star = k.add([
+    k.circle(randomRadius),
+    k.color(255, 255, 255),
+    k.opacity(randomOpacity),
+    k.pos(k.rand(0, k.width()), -50),
+    k.anchor("center"),
+    k.area(),
+    "star"
+  ]);
+  
+  star.onUpdate(() => {
+    star.pos.y += randomSpeed * k.dt();
+    if (star.pos.y > k.height() + 50) star.destroy();
+  });
+  
+});
+
+// Second layer (furthest)
+k.loop(0.025, () => {
+  const randomSpeed = k.rand(250, 300);
+  const randomRadius = k.rand(0.75, 1.25);
+  const randomOpacity = k.rand(0, 0.25);
+  const star = k.add([
+    k.circle(randomRadius),
+    k.color(255, 255, 255),
+    k.opacity(randomOpacity),
+    k.pos(k.rand(0, k.width()), -50),
+    k.anchor("center"),
+    k.area(),
+    "star"
+  ]);
+  
+  star.onUpdate(() => {
+    star.pos.y += randomSpeed * k.dt();
+    if (star.pos.y > k.height() + 50) star.destroy();
+  });
+  
+});
+
+// Third layer (closest)
+k.loop(0.5, () => {
+  const randomSpeed = k.rand(850, 1000);
+  const randomRadius = k.rand(3, 5);
+  const randomOpacity = k.rand(0.5, 0.75);
+  const star = k.add([
+    k.circle(randomRadius),
+    k.color(255, 255, 255),
+    k.opacity(randomOpacity),
+    k.pos(k.rand(0, k.width()), -50),
+    k.anchor("center"),
+    k.area(),
+    "star"
+  ]);
+  
+  star.onUpdate(() => {
+    star.pos.y += randomSpeed * k.dt();
+    if (star.pos.y > k.height() + 50) star.destroy();
+  });
+  
+});
+
+// ------------------------------
+// Items
 // ------------------------------
 k.loop(0.5, () => {
   const startAngle = k.rand(0, 360);
-  const spinSpeed = k.rand(-180, 180);
+  const spinSpeed = k.rand(-120, 120);
   const randomSpeed = k.rand(300, 500);
-  const randomScale = k.rand(0.7, 1.1);
-
-  const randomObstacle = k.chance(0.2) ? "coin" : "steel";
-  const obstacle = k.add([
-    k.sprite(randomObstacle),
+  const randomScale = k.rand(0.75, 1);
+  
+  const randomItem = k.chance(0.2) ? "coin" : k.choose(["rock-sm", "rock-md", "rock-lg"]);
+  const randomTag = randomItem === "coin" ? "coin" : "rock";
+  const anim = randomItem === "coin" ? { anim: "idle" } : {};
+  const item = k.add([
+    k.sprite(randomItem, anim),
     k.pos(k.rand(0, k.width()), -50),
     k.scale(randomScale),
     k.rotate(startAngle),
     k.anchor("center"),
     k.area(),
-    randomObstacle,
-    "obstacle"
+    randomTag,
+    "item"
   ]);
   
   // Drop and spin
-  obstacle.onUpdate(() => {
-    obstacle.pos.y += randomSpeed * k.dt();
-    obstacle.angle += spinSpeed * k.dt();
-    if (obstacle.pos.y > k.height() + 50) obstacle.destroy();
+  item.onUpdate(() => {
+    item.pos.y += randomSpeed * k.dt();
+    item.angle += spinSpeed * k.dt();
+    if (item.pos.y > k.height() + 50) item.destroy();
   });
   
 });
@@ -87,7 +181,7 @@ starr.onCollide("coin", (coin) => {
   addCoins(1);
 });
 
-// Steel
+// Rocks
 const healthBar = document.getElementById("health-bar");
 let health = 100;
 function changeHealth(num) {
@@ -102,7 +196,7 @@ function changeHealth(num) {
 
 }
 
-starr.onCollide("steel", (steel) => {
-  steel.destroy();
+starr.onCollide("rock", (rock) => {
+  rock.destroy();
   changeHealth(-20);
 });
